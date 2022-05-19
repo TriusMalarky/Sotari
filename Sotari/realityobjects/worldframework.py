1b379c349309b994e9ef51ad76e03271330fed00
TriusMalarky <51863464+TriusMalarky@users.noreply.github.com>
2022-05-19 16:55:55 -0600

    World Entities
    World, Continent, Island, Region and Nation classes

---
+++ Sotari/realityobjects/worldframework.py
@@ -0,0 +1,16 @@
+class World:
+    def __init__(self, name):
+        self.name = name
+
+
+class Continent:
+    pass
+
+class Island:
+    pass
+
+class Region:
+    pass
+
+class Nation:
+    pass
