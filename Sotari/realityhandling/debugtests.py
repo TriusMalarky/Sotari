5dc9e1a77496bd6401bd0c58cad239fcd98f3b1d
TriusMalarky <51863464+TriusMalarky@users.noreply.github.com>
2022-05-19 14:57:44 -0600

    Equipment Value Test
    Equipment is successfully created and successfully has value.

--- Sotari/realityhandling/debugtests.py
+++ Sotari/realityhandling/debugtests.py
@@ -1,7 +1,10 @@
-from realityobjects.itemframework import Item
+from realityobjects.itemframework import *
 
 def debugtest():
 
     # Test Items
     chair = Item()
     print(chair.value)
+
+    hat = Equipment()
+    print(hat.value)
