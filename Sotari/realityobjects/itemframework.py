7e9dfd8e335da957a9055bb2e0670341cfa3da95
TriusMalarky <51863464+TriusMalarky@users.noreply.github.com>
2022-05-19 16:47:55 -0600

    Visibility
    Added attribute visibility to class Item

--- Sotari/realityobjects/itemframework.py
+++ Sotari/realityobjects/itemframework.py
@@ -1,7 +1,8 @@
 class Item:
-    def __init__(self, name, value = 0):
+    def __init__(self, name, value = 0, visibility = 0):
         self.name = name
         self.value = value
+        self.visibility = visibility
 
     def set_value(self, new_value):
         self.value = new_value
